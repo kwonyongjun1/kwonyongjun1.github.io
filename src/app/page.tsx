2602352acd4efa062ef8f7123c59ca2eccc5ec7a
kwonyongjun <dydwns18011@gmail.com>
2026-01-05 11:42:26 +0900

    feat: Posts 전체보기 링크 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import ShortAbout from "@/entities/about/ui/ShortAbout";
 import { getAllPost } from "@/entities/post/api/post";
 import PostItem from "@/entities/post/ui/PostItem";
 import Button from "@/shared/ui/Button";
+import Link from "next/link";
 
 const Home = async () => {
   const posts = getAllPost();
@@ -13,7 +14,12 @@ const Home = async () => {
       <div className="flex flex-col gap-md">
         <div className="flex items-center justify-between gap-sm">
           <span className="text-xl font-semibold">Posts</span>
-          <Button text="View All" />
+          <Link
+            href="/posts"
+            className="px-md py-sm bg-app-sub-bg dark:bg-app-dark-sub-bg rounded-md text-app-sub-text dark:text-app-dark-sub-text text-sm"
+          >
+            View All
+          </Link>
         </div>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-md">
           {posts.map((post) => (
